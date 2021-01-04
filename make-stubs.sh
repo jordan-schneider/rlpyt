16a4079223251bfb21e32895ae3c9382f9cf2f1d
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-01-04 16:41:57 -0500

    Updates stubs

--- make-stubs.sh
+++ make-stubs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 rm -rf rlpyt-stubs/*
 git fetch upstream
-git rebase upstream/master
+git rebase -Xours --no-keep-empty --empty=drop upstream/master
 git pull
-git cherry-pick caee25c741ca28b193dd7675a3065fa8a29dbdf5
+git cherry-pick -Xtheirs caee25c741ca28b193dd7675a3065fa8a29dbdf5
+rm -rf rlpyt-stubs/*
 stubgen -o rlpyt-stubs -p rlpyt
 mv rlpyt-stubs/rlpyt/* rlpyt-stubs
-git cherry-pick 47682338eed8c6b94c75bb19f10244788506d3b2
+git add rlpyt-stubs
+git cherry-pick -Xtheirs 47682338eed8c6b94c75bb19f10244788506d3b2
